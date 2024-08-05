dfd4eed34205e40f0a2fffc2d3a346ce61adbc54
Arti <59352535+artifishvr@users.noreply.github.com>
2024-08-04 18:28:59 -0700

    minor fixes

--- src/index.js
+++ src/index.js
@@ -6,45 +6,45 @@ const fallbackimage = 'https://upload.wikimedia.org/wikipedia/commons/4/49/A_bla
 
 router.get('/:videourl+?', ({ params }) => {
 	return html(`
-		<!DOCTYPE html>
-		<html>
-			<head>
-				<title>stolen embeds</title>
-				<meta property="og:image" content="${fallbackimage}" />
-				<meta property="og:type" content="video.other" />
-				<meta property="og:video:url" content="${params.videourl || fallbackvideo}" />
-				<meta property="og:video:width" content="1280" />
-				<meta property="og:video:height" content="720" />
-				<style>
-					body {
-						background-color: #171717;
-						color: #fff;
-						font-family: sans-serif;
-					}
-					a {
-						color: #7dd3fc;
-					}
-				</style>
-			</head>
-			<body>
-				${
-					params.videourl
-						? `<a href="${params.videourl}">link to embedded video</a>
-		<video controls width="1280" height="720">
+  	<!DOCTYPE html>
+  	<html>
+  	<head>
+    	  <title>stolen embeds</title>
+    	  <meta property="og:image" content="${fallbackimage}">
+    	  <meta property="og:type" content="video.other">
+    	  <meta property="og:video:url" content="${params.videourl || fallbackvideo}">
+    	  <meta property="og:video:width" content="1280">
+    	  <meta property="og:video:height" content="720">
+		  <style>
+        	  body {
+        	      background-color: #171717;
+    	          color: #fff;
+				  font-family: sans-serif;
+    	      }
+			  a {
+				  color: #7dd3fc;
+			}
+    	  </style>
+  	</head>
+  	<body>
+	${
+		params.videourl
+			? `<a href="${params.videourl}">link to embedded video</a><br>
+		<video controls width="640" height="360">
     	    <source src="${params.videourl}" type="video/mp4">
     	</video>`
-						: `<h3>use this by putting https://e.nya.llc/ in front of the video URL, like so: https://e.nya.llc/${fallbackvideo}. see more at <a href="https://github.com/artifishvr/StolenEmbeds">artifishvr/StolenEmbeds</a></h3>
-		<video controls width="1280" height="720">
+			: `<h3><a href="https://github.com/artifishvr/StolenEmbeds">artifishvr/StolenEmbeds</a></h3>
+		<video controls width="300" height="300">
     	    <source src="${fallbackvideo}" type="video/mp4">
     	</video>`
-				}
-				<br />
+	}
+    	  <br>
 
-				<h2>embed method from stolen.shoes</h2>
-				<h4>we do not host any of the content embedded or redirected through this site. please take issue with the content hoster.</h4>
-			</body>
-		</html>
-	`);
+    	  <h2>embed method from stolen.shoes</h2>
+		  <h4>we do not host any of the content embedded or redirected through this site. please take issue with the content hoster.</h4>
+  	</body>
+  	</html>
+  	`);
 });
 
 export default { ...router };
