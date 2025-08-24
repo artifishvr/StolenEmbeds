6a297fe93318877d2b6c1be7492d3bc6ca4aee3d
Arti <59352535+artifishvr@users.noreply.github.com>
2025-08-24 05:11:32 -0400

    revert 1c24ca966c634ae669184291953cf8b4c7f7a188

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
-import { IttyRouter, html } from 'itty-router';
+import { IttyRouter, html } from "itty-router";
 const router = IttyRouter();
 
-const fallbackvideo = 'https://rizz.zip/blueberry-NhVxub8W3eof.mp4';
-const fallbackimage = 'https://upload.wikimedia.org/wikipedia/commons/4/49/A_black_image.jpg';
+const fallbackvideo = "https://rizz.zip/blueberry-NhVxub8W3eof.mp4";
+const fallbackimage =
+  "https://upload.wikimedia.org/wikipedia/commons/4/49/A_black_image.jpg";
 
-router.get('/:videourl+?', ({ params, request }) => {
-	const url = new URL(request.url);
-	let videoUrl = params.videourl || '';
-	if (url.search) {
-		videoUrl += url.search;
-	}
-	return html(`
+router.get("/:videourl+?", ({ params, query }) => {
+  if (query) {
+    let first = true;
+    for (const [key, value] of Object.entries(query)) {
+      if (first) {
+        first = false;
+        params.videourl += `?${key}=${value}`;
+        continue;
+      }
+      params.videourl += `&${key}=${value}`;
+    }
+  }
+  return html(`
   	<!DOCTYPE html>
   	<html>
   	<head>
     	  <title>stolen embeds</title>
     	  <meta property="og:image" content="${fallbackimage}">
     	  <meta property="og:type" content="video.other">
-		<meta property="og:video:url" content="${videoUrl || fallbackvideo}">
+    	  <meta property="og:video:url" content="${
+          params.videourl || fallbackvideo
+        }">
     	  <meta property="og:video:width" content="1280">
     	  <meta property="og:video:height" content="720">
 		  <style>
@@ -33,16 +42,16 @@ router.get('/:videourl+?', ({ params, request }) => {
   	</head>
   	<body>
 	${
-		videoUrl
-			? `<a href="${videoUrl}">link to embedded video</a><br>
-	<video controls width="640" height="360">
-		<source src="${videoUrl}" type="video/mp4">
-	</video>`
-			: `<h3><a href="https://github.com/artifishvr/StolenEmbeds">artifishvr/StolenEmbeds</a></h3>
-	<video controls width="300" height="300">
-		<source src="${fallbackvideo}" type="video/mp4">
-	</video>`
-	}
+    params.videourl
+      ? `<a href="${params.videourl}">link to embedded video</a><br>
+		<video controls width="640" height="360">
+    	    <source src="${params.videourl}" type="video/mp4">
+    	</video>`
+      : `<h3><a href="https://github.com/artifishvr/StolenEmbeds">artifishvr/StolenEmbeds</a></h3>
+		<video controls width="300" height="300">
+    	    <source src="${fallbackvideo}" type="video/mp4">
+    	</video>`
+  }
     	  <br>
 
     	  <h2>embed method from stolen.shoes</h2>
